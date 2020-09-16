42a9e77886f1cf7f036c8d79c4cb2cff77627aec
jensengbg-matilda-smedman <59621651+jensengbg-matilda-smedman@users.noreply.github.com>
2020-09-16 19:48:11 +0200

    Add files via upload
    Jag fick ordning på koden nu tillslut :)))

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <router-view 
+    :cards="cards"
+    :cardInfo="cardInfo" 
+    @cardInfo="listenCard"
+    @addCards="addCards"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      cardInfo: {
+        cardNumber: '',
+        cardholderName: '',
+        valid: '',
+        ccv: '',
+        vendor: '',
+      },
+      cards: []
+    }
+  }, 
+  methods: {
+    listenCard(value) {
+      return (this.cardInfo = value), this.addCards();
+    },
+    addCards(){
+      this.cards.push(this.cardInfo);
+      localStorage.setItem('cards',JSON.stringify(this.addCards))
+    }
+  },
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <Top />
+    <h4>ACTIVE CARD</h4>
+    <Card 
+    :cardData="showCard" 
+    />
+    <CardStack 
+    :cards="cards" 
+    :cardInfo="cardInfo" 
+    @activatecard="activateCard"/>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card';
+import Top from '@/components/Top';
+import CardStack from '@/components/CardStack';
+export default {
+  name: 'Home',
+  components: {
+    Top,
+    Card,
+    CardStack
+  },
+data() {
+    return {
+      showCard: 
+        {
+          cardNumber: "",
+          cardholderName: "",
+          valid: "",
+          ccv: "",
+          vendor: "",
+        }
+    }
+  },
+    props: {
+    cardInfo: Object,
+    cards: Array
+  },
+  methods: {
+    activateCard(card) {
+      this.showCard = card
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+  h4{
+    display: flex;
+    justify-content: center;
+    color: gray;
+  }
+</style>
